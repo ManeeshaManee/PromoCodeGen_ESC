c43d89e47ff3ef90ac9a6be78d6af2178c2952a6
ManeeshaManee <maneeshamanee79@gmail.com>
2025-05-11 15:13:39 +0400

    modified settings.py

--- promocode_genapp/promocode_genapp/settings.py
+++ promocode_genapp/promocode_genapp/settings.py
@@ -125,10 +125,3 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maneeshamanee79@gmail.com'
-EMAIL_HOST_PASSWORD = 'gnec fsfb brhi qlsu' 
-DEFAULT_FROM_EMAIL = 'no-reply@gmail.com'
